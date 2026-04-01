df61871ca5f6c2dd83bf60dc41421e38b55c2fb1
carol-xu4 <carol.xu@emory.edu>
2026-04-01 17:26:13 -0400

    mooOo00oO

--- analysis/analysis-code.R
+++ analysis/analysis-code.R
@@ -10,9 +10,17 @@ ppdata = read.csv("data/output/ppdata.csv")
 
 # How many non-elderly workers (aged 18-64) are on ESI.
 ppdata %>% filter(WORKYN == 1) %>%
-    summarise(esi_workers = sum(NOW_GRP == 1))
+    summarise(esi_workers_raw = sum(NOW_GRP == 1))
 
 ppdata %>% filter(WORKYN == 1) %>%
     summarise(esi_workers_pop = sum(MARSUPWT[NOW_GRP == 1]))
+
+# Non-workers on ESI (age 18-64)
+
+# Non-workers on ESI (under age 18)
+
+# Population on ESI by worker status
+
+# Population on ESI by age
 
 #  How many non-elderly workers (aged 18-64) are on ESI from their own employer. 
