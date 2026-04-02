8b226863abb5619951fade4c18948f7b7910e34f
carol-xu4 <carol.xu@emory.edu>
2026-04-02 09:54:41 -0400

    ;p

--- analysis/analysis-code.R
+++ analysis/analysis-code.R
@@ -9,17 +9,26 @@ setwd("C:/Users/CarolXu/OneDrive - Cato Institute/Desktop/CPS ASEC")
 ppdata = read.csv("data/output/ppdata.csv")
 
 # How many non-elderly workers (aged 18-64) are on ESI.
-ppdata %>% filter(WORKYN == 1) %>%
+esi_workers_raw = ppdata %>%
+    filter(A_AGE >= 18 & A_AGE <= 64, WORKYN == 1) %>%
     summarise(esi_workers_raw = sum(NOW_GRP == 1))
 
-ppdata %>% filter(WORKYN == 1) %>%
+esi_workers_pop = ppdata %>% 
+    filter(A_AGE >= 18 & A_AGE <= 64, WORKYN == 1) %>%
     summarise(esi_workers_pop = sum(MARSUPWT[NOW_GRP == 1]))
 
 # Non-workers on ESI (age 18-64)
+esi_nowork_raw = ppdata %>%
+    filter(A_AGE >= 18 & A_AGE <= 64, WORKYN == 2) %>%
+    summarise(esi_nowork_raw = sum(NOW_GRP == 2))
 
 # Non-workers on ESI (under age 18)
+esi_nowork_pop = ppdata %>%
+    filter(A_AGE >= 18 & A_AGE <= 64, WORKYN == 2) %>%
+    summarise(esi_nowork_pop = sum(MARSUPWT[NOW_GRP == 2]))
 
 # Population on ESI by worker status
+
 
 # Population on ESI by age
 
--- data-code/cleaning.r
+++ data-code/cleaning.r
@@ -20,33 +20,32 @@ pppub = read.csv("data/input/pppub25.csv")
 
 # Selecting Variables ------------------------------------------------------
 ppdata = pppub %>% select(
-    PERIDNUM, PH_SEQ, P_SEQ, A_LINENO,              # IDs / structure
+    PERIDNUM, PH_SEQ, P_SEQ, A_LINENO,              # IDs 
     A_AGE, A_SEX, PRDTRACE, PEHSPNON, A_MARITL,     # demographics
     FAMREL, A_FAMREL, A_HGA, A_PFREL,               # family / education
     WORKYN, WRK_CK, PEMLR, CLWK, PEIO1COW,          # employment
     ERN_SRCE, ERN_VAL, PTOTVAL,                     # income
-    COV, COV_MULT_CYR, PRIV, PUB,                   # overall insurance
+    COV, COV_MULT_CYR, PRIV, PUB,                   # any insurance
     GRP, OWNGRP, DEPGRP, OUTGRP,                    # ESI (past year)
     NOW_GRP, NOW_OWNGRP, NOW_DEPGRP,                # ESI (current)
     ESIOFFER, ESICOULD,                             # ESI offer / eligibility
-    ESITAKE1, ESITAKE2, ESITAKE3, ESITAKE4,         # reasons not take 
+    ESITAKE1, ESITAKE2, ESITAKE3, ESITAKE4,         # reasons not take ESI
     ESITAKE5, ESITAKE6, ESITAKE7, ESITAKE8,         
-    PEOFFER, PECOULD,                               # alt offer block
+    PEOFFER, PECOULD,                               # alt offer
     PEWNELIG1, PEWNELIG2, PEWNELIG3, PEWNELIG4,     # alt eligibility / take
     PEWNELIG5,PEWNELIG6, PEWNTAKE1, PEWNTAKE2, PEWNTAKE3,
     PEWNTAKE4, PEWNTAKE5, PEWNTAKE6, PEWNTAKE7, PEWNTAKE8,                         
     DIR, MRK, MRKS, NONM,                           # private non-ESI
     CAID, MCAID, MCARE,                             # public coverage
-    MIL, VACARE, CHAMPVA, PCHIP, IHSFLG,            # other public
+    MIL, VACARE, CHAMPVA, PCHIP, IHSFLG,            # other public coverage
     HEA,                                            # health
-    MOOP,                                           # spending
-    PERIDNUM, MARSUPWT                              # id & weight
+    MOOP,                                           # health spending
+    PERIDNUM, MARSUPWT                              # weight
 )
 
 # Recode MARSUPWT variable (2 implied decimals) & Filter age 18-64 
 ppdata = ppdata %>%
-    mutate(MARSUPWT = MARSUPWT / 100) %>%
-    filter(A_AGE >= 18 & A_AGE <= 64)
+    mutate(MARSUPWT = MARSUPWT / 100) 
 
 # Rewrite to Output 
 write_csv(ppdata, "data/output/ppdata.csv")
